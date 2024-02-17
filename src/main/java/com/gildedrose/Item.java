035168da0cf52a5c5702b7c4a5c2dcc9f32fa52c
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2024-02-17 12:21:09 -0300

    3. Usando Move Method muevo los tres métodos que extraje a la clase Item. ¿Cómo me doy cuenta que van ahi? Si observamos bien, cada uno de éstos métodos recibian por parametro a una instancia de item para trabajar con ella. La clase Item tiene todas propiedades públicas que se modificaban desde éstos métodos, cuando es el mismo Item el que deberia actuar sobre su propio estado interno y honrar el encapsulamiento.

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -15,4 +15,57 @@ public class Item {
     public String toString() {
         return this.nombre + ", " + this.expiraEn + ", " + this.calidad;
     }
+
+    void administrarCalidad() {
+        if (!nombre.equals("Aged Brie")
+                && !nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (calidad > 0) {
+                if (!nombre.equals("Sulfuras, Hand of Ragnaros")) {
+                    calidad = calidad - 1;
+                }
+            }
+        } else {
+            if (calidad < 50) {
+                calidad = calidad + 1;
+                if (nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (expiraEn < 11) {
+                        if (calidad < 50) {
+                            calidad = calidad + 1;
+                        }
+                    }
+                    if (expiraEn < 6) {
+                        if (calidad < 50) {
+                            calidad = calidad + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void administrarCaducidad() {
+        if (!nombre.equals("Sulfuras, Hand of Ragnaros")) {
+            expiraEn = expiraEn - 1;
+        }
+    }
+
+    void administrarCalidadArticulosExpirados() {
+        if (expiraEn < 0) {
+            if (!nombre.equals("Aged Brie")) {
+                if (!nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (calidad > 0) {
+                        if (!nombre.equals("Sulfuras, Hand of Ragnaros")) {
+                            calidad = calidad - 1;
+                        }
+                    }
+                } else {
+                    calidad = 0;
+                }
+            } else {
+                if (calidad < 50) {
+                    calidad = calidad + 1;
+                }
+            }
+        }
+    }
 }
