1f7d4aa9b22e8d274c77a7dd9aec732d6c6fff35
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2024-02-17 12:15:45 -0300

    2. Usando Extract Method le doy semántica a los 3 bloques de código principales dentro del método updateQuality.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,50 +9,60 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.nombre.equals("Aged Brie")
-                    && !item.nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.calidad > 0) {
-                    if (!item.nombre.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.calidad = item.calidad - 1;
+            administrarCalidad(item);
+            administrarCaducidad(item);
+            administrarCalidadArticulosExpirados(item);
+        }
+    }
+
+    private void administrarCalidad(Item item) {
+        if (!item.nombre.equals("Aged Brie")
+                && !item.nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.calidad > 0) {
+                if (!item.nombre.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.calidad = item.calidad - 1;
+                }
+            }
+        } else {
+            if (item.calidad < 50) {
+                item.calidad = item.calidad + 1;
+                if (item.nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.expiraEn < 11) {
+                        if (item.calidad < 50) {
+                            item.calidad = item.calidad + 1;
+                        }
                     }
+                    if (item.expiraEn < 6) {
+                        if (item.calidad < 50) {
+                            item.calidad = item.calidad + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void administrarCaducidad(Item item) {
+        if (!item.nombre.equals("Sulfuras, Hand of Ragnaros")) {
+            item.expiraEn = item.expiraEn - 1;
+        }
+    }
+
+    private void administrarCalidadArticulosExpirados(Item item) {
+        if (item.expiraEn < 0) {
+            if (!item.nombre.equals("Aged Brie")) {
+                if (!item.nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.calidad > 0) {
+                        if (!item.nombre.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.calidad = item.calidad - 1;
+                        }
+                    }
+                } else {
+                    item.calidad = 0;
                 }
             } else {
                 if (item.calidad < 50) {
                     item.calidad = item.calidad + 1;
-
-                    if (item.nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.expiraEn < 11) {
-                            if (item.calidad < 50) {
-                                item.calidad = item.calidad + 1;
-                            }
-                        }
-
-                        if (item.expiraEn < 6) {
-                            if (item.calidad < 50) {
-                                item.calidad = item.calidad + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            if (!item.nombre.equals("Sulfuras, Hand of Ragnaros")) {
-                item.expiraEn = item.expiraEn - 1;
-            }
-            if (item.expiraEn < 0) {
-                if (!item.nombre.equals("Aged Brie")) {
-                    if (!item.nombre.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.calidad > 0) {
-                            if (!item.nombre.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.calidad = item.calidad - 1;
-                            }
-                        }
-                    } else {
-                        item.calidad = 0;
-                    }
-                } else {
-                    if (item.calidad < 50) {
-                        item.calidad = item.calidad + 1;
-                    }
                 }
             }
         }
